f1c80263fa3138868ddefe779078a3e88965d152
Thomas Maffezzolli <thomasmaffezzolli8@gmail.com>
2025-02-26 20:56:02 -0300

    ✨ Add generatePairArray function to create pairs from permutations

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"math/rand/v2"
+)
+
 func permute(nums, current []int, used []bool, results *[][]int) {
 
 	if len(current) == len(nums) {
 		temp := make([]int, len(current))
 		copy(temp, current)
 		*results = append(*results, temp)
-		return
-	}
-
-	if len(*results) == 10 {
 		return
 	}
 
@@ -26,6 +27,22 @@ func permute(nums, current []int, used []bool, results *[][]int) {
 	}
 }
 
+func generatePairArray(results [][]int) [][]int {
+	pn := make([][]int, 5)
+
+	t := results[rand.IntN(len(results))]
+
+	aux := 0
+	for i := range 5 {
+		pn[i] = make([]int, 2)
+		pn[i][0] = t[aux]
+		pn[i][1] = t[aux+1]
+		aux += 2
+	}
+
+	return pn
+}
+
 func main() {
 	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
 
@@ -34,4 +51,6 @@ func main() {
 	used := make([]bool, len(numbers))
 
 	permute(numbers, []int{}, used, &results)
+
+	fmt.Println(generatePairArray(results))
 }
